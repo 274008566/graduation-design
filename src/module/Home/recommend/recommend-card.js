8b87f638422b292c16ecdaa134defd4e8973a16f
duxue <1479605026@qq.com>
2020-02-18 21:48:50 +0800

    fix

--- src/module/Home/recommend/recommend-card.js
+++ src/module/Home/recommend/recommend-card.js
@@ -7,7 +7,7 @@ export class RecommendCard extends Component {
                 <a href="#">
                     <img src={require('../../../assets/images/recommend1.jpg')}/>
                     <h3>哈佛大学用这12张图，教育了10亿人_心灵鸡汤</h3>
-                    <p class="muted">很多人把感恩当成一种付出，其实感恩本身就是一种幸福。...</p>
+                    <p className="muted">很多人把感恩当成一种付出，其实感恩本身就是一种幸福。...</p>
                 </a>
             </div>
         );
